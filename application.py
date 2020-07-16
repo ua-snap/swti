35be01420241c11fdc05c74ec83e68d6a952e191
Bob Torgerson <rltorgerson@alaska.edu>
2020-07-16 14:05:06 -0800

    Fixes bug with autorange for range slider.

--- application.py
+++ application.py
@@ -86,7 +86,7 @@ def update_daily_index(nonce):  # deliberate unused arg
                 showgrid=True,
                 type="date",
                 range=[start_date, end_date],
-                rangeslider=dict(visible=True),
+                rangeslider=dict(range=[di["date"].iloc[0], di["date"].iloc[-1]], visible=True),
             ),
         ),
     )
