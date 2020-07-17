e5fc9346912075b1f40f3a43c408e25c78a64cee
Bruce Crevensten <bruce.crevensten@gmail.com>
2020-07-17 15:09:45 -0700

    Fix quotes, rephrase explainer on +/- 3 lines

--- gui.py
+++ gui.py
@@ -74,21 +74,20 @@ about = wrap_in_section(
         ddsih.DangerouslySetInnerHTML(
             f"""
 <h1 class="title is-3">Statewide Temperature Index</h1>
-<p>&#34;Has it been cold lately in Alaska?&#34; Answer: &#34;It&#39;s complicated.&#34; Why? Alaska is a very large 
-region with complex geography and sparse data availability. This tool develops a statewide temperature index, 
-a simple indicator which balances accessible information on temperature variation with the complexity of the data.</p> 
+<p>&ldquo;Has it been cold lately in Alaska?&rdquo; Answer: &ldquo;It&rsquo;s complicated.&rdquo; Why? Alaska is a very large
+region with complex geography and sparse data availability. This tool develops a statewide temperature index,
+a simple indicator which balances accessible information on temperature variation with the complexity of the data.</p>
 <p>The chart below graphs the average temperature across Alaska each day, and compares it to the historical average.
 The line marked at 0 represents the average normal historical temperature. Each dot represents the average
 temperature across Alaska for that day.</p>
 <ul>
-    <li>Red dots indicate &#34;warmer than normal&#34; temperatures. Blue dots indicate “colder than normal.” </li>
-    <li>The distance above or below the &#34;normal&#34; line represents the amount of deviation from normal. A value 
-of +1, for instance, means that the day is warmer than 10% of all above-normal days.</li> 
+    <li>Red dots indicate &ldquo;warmer than normal&rdquo; temperatures. Blue dots indicate &ldquo;colder than normal.&rdquo; </li>
+    <li>The distance above or below the &ldquo;normal&rdquo; line represents the amount of deviation from normal. A value
+of +1, for instance, means that the day is warmer than 10% of all above-normal days.</li>
     <li>The black line represents a running 30-day average. This line is less affected by short-term (1-3 day)
 temperature anomalies.</li>
-    <li>The gray dashed lines at three degrees above and below the “normal” line represent typical daily temperature 
-fluctuation. Daily index values within these lines are unexceptional, while values above or below these lines 
-represent extreme temperature variation.</li> 
+    <li>The gray dotted lines at +3 and -3 represent the typical range of the index. Daily index values within these lines are unexceptional, while values above or below these lines
+represent extreme temperature variation.</li>
     <li><p class="camera-icon">Click the <span>
 <svg viewBox="0 0 1000 1000" class="icon" height="1em" width="1em"><path d="m500 450c-83 0-150-67-150-150 0-83 67-150 150-150 83 0 150 67 150 150 0 83-67 150-150 150z m400 150h-120c-16 0-34 13-39 29l-31 93c-6 15-23 28-40 28h-340c-16 0-34-13-39-28l-31-94c-6-15-23-28-40-28h-120c-55 0-100-45-100-100v-450c0-55 45-100 100-100h800c55 0 100 45 100 100v450c0 55-45 100-100 100z m-400-550c-138 0-250 112-250 250 0 138 112 250 250 250 138 0 250-112 250-250 0-138-112-250-250-250z m365 380c-19 0-35 16-35 35 0 19 16 35 35 35 19 0 35-16 35-35 0-19-16-35-35-35z" transform="matrix(1 0 0 -1 0 850)"></path></svg>
 </span> icon in the upper&ndash;right of each chart to download it.</p></li>
