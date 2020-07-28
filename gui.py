eeeb65366ac7771c858726a41cdf03e88829f025
Bob Torgerson <rltorgerson@alaska.edu>
2020-07-28 10:54:25 -0800

    Required change to UAF.svg path in footer for AWS EB instance.

--- gui.py
+++ gui.py
@@ -193,7 +193,7 @@ footer = html.Footer(
             f"""
 <div class="container">
     <div class="wrapper is-size-6">
-        <img src="assets/UAF.svg"/>
+        <img src="{path_prefix}assets/UAF.svg"/>
         <div class="wrapped">
             <p>The Alaska Statewide Temperature Index was developed by Rick Thoman and Brian Brettschneider from data provided by the National Weather Service ASOS system. This website was developed by the <a href="https://uaf-accap.org/">Alaska Center for Climate Assessment and Policy (ACCAP)</a> and the <a href="https://www.snap.uaf.edu/" title="👍">Scenarios Network for Alaska and Arctic Planning (SNAP)</a>, research groups at the <a href="https://uaf-iarc.org/">International Arctic Research Center (IARC)</a> at the <a href="https://uaf.edu/uaf/">University of Alaska Fairbanks (UAF)</a>.</p>
             <p>Copyright &copy; {current_year} University of Alaska Fairbanks.  All rights reserved.</p>
