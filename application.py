7841605e0c3b0327e9956803e6ea233241e0bb4d
Craig Stephenson <crstephenson@alaska.edu>
2022-12-05 14:38:52 -0900

    Abbreviate month in x-axis tick labels to fix exported PNG overlap bug.

--- application.py
+++ application.py
@@ -97,6 +97,7 @@ def update_daily_index(nonce):  # deliberate unused arg
             xaxis=dict(
                 showgrid=True,
                 type="date",
+                tickformat='%b %-d, %Y',
                 range=[start_date, end_date],
                 rangeslider=dict(
                     range=[di["date"].iloc[0], di["date"].iloc[-1]], visible=True
