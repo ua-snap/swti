1e713e4e38161af4eb0d24adc792823a8e28100c
Bob Torgerson <rltorgerson@alaska.edu>
2026-02-24 11:35:04 -0900

    Updates ACCAP logo and name in the web application.

--- gui.py
+++ gui.py
@@ -176,7 +176,7 @@ footer = html.Footer(
     <div class="wrapper is-size-6">
         <img src="{path_prefix}assets/UAF.svg"/>
         <div class="wrapped">
-            <p>The Alaska Statewide Temperature Index was developed by Rick Thoman and Brian Brettschneider from data provided by the National Weather Service ASOS system. This website was developed by the <a href="https://uaf-accap.org/">Alaska Center for Climate Assessment and Policy (ACCAP)</a> and the <a href="https://www.snap.uaf.edu/" title="👍">Scenarios Network for Alaska and Arctic Planning (SNAP)</a>, research groups at the <a href="https://uaf-iarc.org/">International Arctic Research Center (IARC)</a> at the <a href="https://uaf.edu/uaf/">University of Alaska Fairbanks (UAF)</a>.</p>
+            <p>The Alaska Statewide Temperature Index was developed by Rick Thoman and Brian Brettschneider from data provided by the National Weather Service ASOS system. This website was developed by the <a href="https://uaf-accap.org/">Alaska Center for Climate Assessment and Preparedness (ACCAP)</a> and the <a href="https://www.snap.uaf.edu/" title="👍">Scenarios Network for Alaska and Arctic Planning (SNAP)</a>, research groups at the <a href="https://uaf-iarc.org/">International Arctic Research Center (IARC)</a> at the <a href="https://uaf.edu/uaf/">University of Alaska Fairbanks (UAF)</a>.</p>
             <p>Copyright &copy; {current_year} University of Alaska Fairbanks.  All rights reserved.</p>
             <p>The <a href="https://www.alaska.edu/">University of Alaska</a> is an
             Equal Opportunity/Equal Access Employer and Educational Institution. The
